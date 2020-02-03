359b072432911f44f3735f8b36aec6ff511086a0
nonepostlife <43952761+nonepostlife@users.noreply.github.com>
2020-02-04 09:12:26 +1000

    Initial commit

---
+++ src/main/java/com/postlife/computeruniverse/custatapi/CustatapiApplication.java
@@ -0,0 +1,13 @@
+package com.postlife.computeruniverse.custatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustatapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustatapiApplication.class, args);
+    }
+
+}
